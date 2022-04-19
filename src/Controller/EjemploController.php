e07805466832a5702f663145586ae236dea96239
ruben Rodriguez <cryptomentsnft@gmail.com>
2022-04-19 15:48:11 +0200

    Cambio para hacer funcionar bd clevercloud 19/04/2022

--- src/Controller/EjemploController.php
+++ src/Controller/EjemploController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Clientes;
 
 class EjemploController extends AbstractController
 {
@@ -13,8 +14,19 @@ class EjemploController extends AbstractController
      */
     public function index(): Response
     {
+        /* insertar en bd */
+        /*  $cli = new Clientes();
+        $cli->setNombre('PruebaInsert');
+        $cli->setPass('PruebaInsert');
+        $cli->setEmail('PruebaInsert');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($cli);
+        $em->flush(); */
+        $em = $this->getDoctrine()->getManager();
+        $prueba = $this->getDoctrine()->getRepository(Clientes::class)->findOneBy(array('id' => 2));
+        /* $single_user = $em->getRepository()->find('Clientes', 1); */
         return $this->render('ejemplo/index.html.twig', [
-            'controller_name' => 'EjemploController',
+            'prueba' => $prueba,
         ]);
     }
 }
